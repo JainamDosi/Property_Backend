fae8803b3ccd3d2baad5b2b9faa392de057bc96d
Jainam <127018175+JainamDosi@users.noreply.github.com>
2025-06-01 13:15:42 +0530

    fixing redis connection

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,5 +1,5 @@
 import Redis from 'ioredis';
-const url = process.env.REDIS_URL || 'redis://localhost:6379';
-console.log('Connecting to Redis at', url);
-const redis = new Redis(url);
+
+console.log('Connecting to Redis ');
+const redis = new Redis(process.env.REDIS_URL as string);
 export default redis;
