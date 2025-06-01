5ecec73be8e757086fdc29b6681f2963a7fb220e
Jainam <127018175+JainamDosi@users.noreply.github.com>
2025-06-01 13:35:05 +0530

    redis connection

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,5 +1,6 @@
 import Redis from 'ioredis';
 
 console.log('Connecting to Redis ');
-const redis = new Redis(process.env.REDIS_URL as string);
+const redis = new Redis(process.env.REDIS_URL!, {
+  tls: {},});
 export default redis;
